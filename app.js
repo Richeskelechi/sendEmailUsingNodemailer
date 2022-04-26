931981b06ff37861b92098712a295dbb279797c1
Riches Kelechi <richeskelechi94.rk@gmail.com>
2022-04-26 12:17:30 +0100

    Modified

--- app.js
+++ app.js
@@ -4,8 +4,13 @@ const sendMail = require('./routes/index');
 const cors = require('cors')
 
 const app = express()
-app.use(cors())
-// app.options('*', cors())
+app.use(cors({
+    origin: '*',
+    credentials: true,
+    allowedHeaders: ['Content-Type', 'Authorization', 'Content-Length', 'X-Requested-With', 'Accept'],
+    methods: ['GET', 'PUT', 'POST', 'DELETE', 'OPTIONS'],
+    optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204 
+}));
 app.use(express.json())
 
 app.use("/test", (req, res) => {
--- controllers/index.js
+++ controllers/index.js
@@ -1,6 +1,8 @@
 const nodemailer = require('nodemailer');
 let mailTransporter = nodemailer.createTransport({
-    service: "gmail",
+    host: 'smtp.gmail.com',
+    port: 465,
+    secure: true,
     auth: {
         user: process.env.gmail,
         pass: process.env.password
